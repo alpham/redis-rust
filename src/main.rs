83cfa9cbfc202448d271b18a9b5374c01e4dcc2d
Ahmed Magdy <ahmed.magdy40@gmail.com>
2024-03-18 01:08:05 +0100

    stage_2: Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use std::net::TcpListener;
+use std::error::Error;
+use std::{
+    net::{TcpListener, TcpStream},
+    io::{BufRead, Write, BufReader},
+};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
     
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_client(stream)?;
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
+    Ok(())
+}
+
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>>{
+    let mut buf = BufReader::new(&stream);
+    let mut line = String::new();
+    buf.read_line(&mut line)?;
+
+    stream.write_all(b"+PONG\r\n").expect("Failed to write to client");
+    Ok(())
 }
