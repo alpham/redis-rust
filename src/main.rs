ee4e0d33fe50d87b77ddd562e47079ef38748a23
Ahmed Magdy <ahmed.magdy40@gmail.com>
2024-03-20 04:12:47 +0100

    stage_3: responde to multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,31 @@
 use std::error::Error;
 use std::{
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
-    io::{BufRead, Write, BufReader},
 };
 
-fn main() -> Result<(), Box<dyn Error>>{
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+fn main() -> Result<(), Box<dyn Error>> {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                handle_client(stream)?;
+                handle_client(stream);
             }
             Err(e) => {
                 eprintln!("error: {}", e);
             }
         }
-    }
+    };
     Ok(())
 }
 
-
-fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>>{
-    let mut buf = BufReader::new(&stream);
-    let mut line = String::new();
-    buf.read_line(&mut line)?;
-
-    stream.write_all(b"+PONG\r\n").expect("Failed to write to client");
-    Ok(())
+fn handle_client(mut stream: TcpStream) {
+    let data = "+PONG\r\n";
+    let mut buf = [0u8;255];
+    while let Ok(_) = stream.read(&mut buf) {
+        stream.write_all(data.as_bytes()).unwrap();
+    }
 }
